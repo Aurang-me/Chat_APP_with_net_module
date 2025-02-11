d6005df94a166d5cded7c412eab4470fa7f7cd49
Aurang-me <aurang.me@gmail.com>
2025-02-11 11:00:32 +0500

    Basic chat app with net module

---
+++ server.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+const server = net.createServer();
+
+const clients = [];
+
+server.on("connection", (socket) => {
+    console.log("A new connection to server!!!");
+
+    socket.on("data", (data) => {
+
+        clients.map((socket) => {
+            socket.write(data);
+        });
+    });
+
+    clients.push(socket);
+});
+
+server.listen(3008, "127.0.0.1", () => {
+    console.log("server is listening at ", server.address());
+})
