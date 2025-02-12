9baba1948a44d82059afa0d3b75a3b12bc9aa1ce
Aurang-me <aurang.me@gmail.com>
2025-02-12 22:39:45 +0500

    improved console for better interaction

--- client.js
+++ client.js
@@ -6,17 +6,47 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const client = net.createConnection({ host: "127.0.0.1", port: 3008 }, async () => {
-    console.log("Connected to server.");
+const client = net.createConnection(
+    { host: "127.0.0.1", port: 3008 },
+    async () => {
+        console.log("Connected to server.");
+        const ask = async () => {
+            const message = await rl.question("Enter a message >");
+            await moveCursor(0, -1);
 
-    const message = await rl.question("Enter a message >");
+            await clearLine(0)
 
-    client.write(message);
-});
+            client.write(message);
+        }
+        ask();
 
-client.on("data", (data) => {
-    console.log(data.toString("utf-8"));
-})
+        client.on("data", async (data) => {
+            console.log();
+            await moveCursor(0, -1);
+            await clearLine(0);
+            console.log(data.toString("utf-8"));
+            ask();
+        })
+
+
+    });
+
+
+
+const clearLine = (dir) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.clearLine(dir, () => {
+            resolve();
+        })
+    })
+}
+const moveCursor = (dx, dy) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.moveCursor(dx, dy, () => {
+            resolve();
+        })
+    })
+}
 
 client.on("end", () => {
     console.log("Disconnected from server")
